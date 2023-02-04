8dfe53f03e69b413a0191614e35c4369eb75e84c
melkecelioglu <67334822+melkecelioglu@users.noreply.github.com>
2023-02-04 23:48:05 +0100

    sol last

--- restricted-access-solutions.sol
+++ restricted-access-solutions.sol
@@ -1,23 +1,5 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
-/*
-Restricted Access to a Contract is a common practice. 
-
-By Default, a contract state is read-only unless it is specified as public.
-
-We can restrict who can modify the contract's state or call a contract's functions using modifiers. 
-
-Let us build a contract with modified restricted access with the following common writeouts:
-
-    onlyBy − only the mentioned caller can call this function.
-
-    onlyAfter − called after certain time period.
-
-    costs − call this function only if certain value is provided.
-
-
-*/
-
 
 
 contract RestrictedAcces {
@@ -25,10 +7,7 @@ contract RestrictedAcces {
     address public owner = msg.sender;
     uint public creationTime = block.timestamp;
     
-    // write a modifier called onlyBy that will require the current caller to be equal to 
-    // an account address which we set as an input in the modifier and if 
-    // the requirement fails send a message that says - Sender not authorized!
-    
+
     modifier onlyBy(address _account) {
         require(msg.sender == _account,
         'Sender not authorized!'
@@ -62,19 +41,4 @@ contract RestrictedAcces {
     function forceOwnerChange(address _newOwner) payable public costs(200 ether) {
         owner = _newOwner;
     }
-    
-    /*
-    Exercise: 
-    
-    1. Create a modifier called costs which takes an _amount parameter
-    2. Require that msg.value is greater than or equal to the amount 
-    3. If the msg.value is not greather than or equal to the amount 
-       return a string that says Not enough Ether provided. 
-    4. Write a funciton called forceOwnerChange which takes an address called _newOwner and is payable 
-    5. add a modification to the signature so that the function needs 200 ether to execute 
-    6. set the owner of the contract to the new owner of the address.
-    
-    7. BONUS****** Figure out how to make the function actually run! 
-    */
-    
 }
